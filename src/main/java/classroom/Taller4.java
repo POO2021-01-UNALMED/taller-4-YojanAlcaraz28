e217af077687c104290ca066b03523791200964c
YojanAlcaraz28 <79714839+YojanAlcaraz28@users.noreply.github.com>
2021-03-23 20:44:30 -0500

    Modificación de código
    Se modifican las clases Asignatura, Grupo, Persona.

--- src/main/java/classroom/Taller4.java
+++ src/main/java/classroom/Taller4.java
@@ -19,11 +19,11 @@ public class Taller4 {
         grupo2.cambiarEstudiante(0, estudiantes1[0]);
         grupo2.cambiarEstudiante(1, estudiantes1[2]);
         grupo2.cambiarEstudiante(estudiantes1[0], estudiantes1[1]);
-
+        
         System.out.println(new Asignatura().nombre);
         System.out.println(grupo2.codigo);
         System.out.println(asignatura2.codigoExterno);
-        asignatura1.cambiarDatos(50);
+        asignatura1.cambiarDatosExt(50);
         System.out.println(asignatura1.codigoExterno);
         System.out.println(estudiantes1[1].cedula);
         System.out.println(new Persona("Alejandro").cedula);
